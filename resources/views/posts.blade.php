3bb293a9aa48d18b8455ed7db6942c44accf3e66
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-10-20 23:16:28 +0100

    Basic

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>Laravel</title>
+</head>
+<body>
+<article>
+    <h1>My First Post</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iste officiis possimus cumque sit velit quasi sint
+        iusto saepe suscipit dicta, laudantium consectetur, rerum sunt, ducimus nobis tenetur aliquid. Saepe delectus
+        voluptates minus ipsa impedit obcaecati earum fugiat corporis dolorem? Unde deleniti possimus illum iure esse
+        cum iusto dignissimos dolor aliquid maiores, nihil obcaecati magnam vel fugit delectus nesciunt in repellat
+        nobis id voluptatibus commodi ratione? Minus magni facere rerum omnis fugiat nostrum eligendi suscipit excepturi
+        quaerat. Commodi quasi unde, rerum recusandae doloremque sint sed repellat delectus eaque obcaecati tempore
+        animi deleniti, blanditiis amet quae, consequuntur nisi in eveniet aspernatur.</p>
+</article>
+
+<article>
+    <h1>My Second Post</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iste officiis possimus cumque sit velit quasi sint
+        iusto saepe suscipit dicta, laudantium consectetur, rerum sunt, ducimus nobis tenetur aliquid. Saepe delectus
+        voluptates minus ipsa impedit obcaecati earum fugiat corporis dolorem? Unde deleniti possimus illum iure esse
+        cum iusto dignissimos dolor aliquid maiores, nihil obcaecati magnam vel fugit delectus nesciunt in repellat
+        nobis id voluptatibus commodi ratione? Minus magni facere rerum omnis fugiat nostrum eligendi suscipit excepturi
+        quaerat. Commodi quasi unde, rerum recusandae doloremque sint sed repellat delectus eaque obcaecati tempore
+        animi deleniti, blanditiis amet quae, consequuntur nisi in eveniet aspernatur.</p>
+</article>
+</body>
+</html>
