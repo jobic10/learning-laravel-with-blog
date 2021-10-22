b52c37621e30effbf5487fa27a344f4e44590552
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-10-22 14:41:10 +0100

    Refactored

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,13 +24,6 @@ class Post
         $this->slug = $slug;
     }
 
-    public static function find($slug)
-    {
-        if (!file_exists($path = (resource_path("posts/{$slug}.html")))) {
-            throw new ModelNotFoundException();
-        }
-        return cache()->remember("post.{$slug}", now()->addSecond(3), fn () => file_get_contents($path));
-    }
 
     public static function all()
     {
@@ -39,5 +32,9 @@ class Post
         )->map(
             fn ($doc) => new Post($doc->title, $doc->excerpt, $doc->date, $doc->slug, $doc->body())
         );
+    }
+    public static function find($slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
     }
 }
