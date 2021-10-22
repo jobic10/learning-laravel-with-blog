bd78ee1fd842f1ae3d449b64fdf0a89eb748cb83
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-10-22 15:55:00 +0100

    ⚡️

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::get('/', function () {
 
 Route::get('post/{post}', function ($slug) {
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
     ]);
 });
