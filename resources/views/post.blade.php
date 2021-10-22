7dfc29a155a5b9d975b54afed59d2b3ffdf2cbfa
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-10-22 15:26:51 +0100

    Layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app.css">
-    <title>{{ $post->title }}</title>
-</head>
-<body>
+@extends('layout')
+@section('content')
 <article>
 <h1>
 {{ $post->title }}
@@ -19,3 +11,4 @@
 <a href="/">Go back</a>
 </body>
 </html>
+@endsection
