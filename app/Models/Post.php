44985f05eb079e46dbfc0187c0cda6691ddcbae0
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-11-05 21:29:11 +0100

    Guarded

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title'];
+    // protected $fillable = ['title'];
+    protected $guarded = ['id'];
 }
