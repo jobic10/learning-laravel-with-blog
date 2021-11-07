2f87a81e5bfa7932bf50e422e2f1df8e2a9acd1e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-11-07 17:07:31 +0100

    Latest

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
-    $posts = Post::with('category', 'user')->get();
+    $posts = Post::latest()->with('category', 'user')->get();
 
     return view('posts', ['posts' => $posts]);
 });
