c338f2e2dbf4bfd3017a077e6e73d3f3665365d5
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-11-07 16:59:40 +0100

    🔥

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
 {{ $post->title }}
 </h1>
 <p>
-        <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+       By <a href="/#">{{ $post->user->name }}</a> in  <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>
     </p>
 <p>
 {!! $post->body !!}
