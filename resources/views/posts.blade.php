5d9d4f0c8a5180ae2922319beb17015d702b5a32
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-11-08 10:49:02 +0100

    Refactor

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,18 +10,18 @@
 <main class="max-w-6xl mx-auto mt-6 space-y-6 lg:mt-20">
 
 
-<x-post-featured-card></x-post-featured-card>
+<x-post-featured-card />
 
 
     <div class="lg:grid lg:grid-cols-2">
-        <x-post-card></x-post-card>
-     <x-post-card></x-post-card>
+        <x-post-card/>
+     <x-post-card/>
     </div>
 
     <div class="lg:grid lg:grid-cols-3">
-        <x-post-card></x-post-card>
-        <x-post-card></x-post-card>
-        <x-post-card></x-post-card>
+        <x-post-card/>
+        <x-post-card/>
+        <x-post-card/>
 
 
     </div>
