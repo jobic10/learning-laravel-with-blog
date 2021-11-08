195038c6188825d270b4173b8c1534e3a44d06f6
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-11-08 11:17:35 +0100

    Conditional

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,8 +8,7 @@
 @include('_posts-header')
 
 <main class="max-w-6xl mx-auto mt-6 space-y-6 lg:mt-20">
-
-
+@if ($posts->count())
 <x-post-featured-card :post="$posts[0]"/>
 
 
@@ -19,5 +18,7 @@
 @endforeach
     </div>
 
-
+@else
+    <p class="text-center">No posts yet. Check back later!</p>
+    @endif
 </main>
