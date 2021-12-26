99605de45224fb9c3830373a58a98a505aba1eec
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-12-26 18:08:31 +0100

    Refactor

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -10,10 +10,7 @@
         <div class="flex flex-col justify-between flex-1">
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="/category/{{ $post->category->slug }}"
-                        class="px-3 py-1 text-xs font-semibold text-blue-300 uppercase border border-blue-300 rounded-full"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
-
+                    <x-category-button :category="$post->category" />
 
                 </div>
 
