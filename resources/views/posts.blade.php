72662dabb2f0dc72523a51c4ac20960a8784f468
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-12-26 05:24:44 +0100

    Refactor

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,15 +10,7 @@
 
 <main class="max-w-6xl mx-auto mt-6 space-y-6 lg:mt-20">
     @if ($posts->count())
-    <x-post-featured-card :post="$posts[0]" />
-
-    @if ($posts->count() > 2)
-    <div class="lg:grid lg:grid-cols-6">
-        @foreach ($posts->skip(1) as $post)
-        <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2'}}" />
-        @endforeach
-    </div>
-    @endif
+    <x-posts-grid :posts="$posts"></x-posts-grid>
     @else
     <p class="text-center">No posts yet. Check back later!</p>
     @endif
