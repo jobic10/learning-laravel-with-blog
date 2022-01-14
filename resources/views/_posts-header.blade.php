c2987decaaf2d760b55c506a01f3dc2873b99c7a
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-01-15 00:09:52 +0100

    DropDown Arrow Component Extracted

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -17,16 +17,7 @@
                 @slot('trigger')
                 <button class="inline-flex flex lg:inline-flex w-32 py-2 pr-9  pl-3 text-sm font-semibold">
                     {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
-                    <svg class="absolute transform -rotate-90 pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                            </path>
-                        </g>
-                    </svg>
+                    <x-down-arrow style="right: 12px;" class="absolute pointer-events-none"></x-down-arrow>
                 </button>
                 @endslot
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
