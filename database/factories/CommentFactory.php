5cdf122cad6f3085e978ccea7cf53c2378e02e51
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-01-21 15:17:13 +0100

    Factory for comment Relationships

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -14,7 +16,9 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body' => $this->faker->paragraph(),
+            'post_id' => Post::factory(),
+            'user_id' => Post::factory(),
         ];
     }
 }
