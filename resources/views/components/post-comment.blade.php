9a737dfc7838aaa05bcd5fd4bd9a93257e7c34ea
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-01-23 05:10:38 +0100

    Removed duplicated styling Used a component instead

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,15 +1,19 @@
 @props(['comment'])
-<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" class="rounded-xl" srcset="">
-    </div>
-    <div class="">
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></time></p>
-        </header>
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+
+<x-panel>
+    <article class="flex bg-gray-100 p-6 space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" class="rounded-xl"
+                srcset="">
+        </div>
+        <div class="">
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></time></p>
+            </header>
+            <p>
+                {{ $comment->body }}
+            </p>
+        </div>
+    </article>
+</x-panel>
