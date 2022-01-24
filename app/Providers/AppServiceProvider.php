a7e6e1d7d82409c7c1fb32bb7236f5f42548add7
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-01-24 22:48:51 +0100

    Remove mass assignments restrictions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // Paginator::useBootstrap();
+        Model::unguard();
     }
 }
